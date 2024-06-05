6c70a4208efaf89664e3bd2168159bbb46d66cf8
jiayuezhang84 <josiezhang84@gmail.com>
2024-06-05 14:46:20 -0400

    a2 CSS and Bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
@@ -13,4 +13,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
-reportWebVitals();
+
